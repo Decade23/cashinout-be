d37665fe75ca8dee5c48fb7bad2040d14119dc1d
Dedi Fardiyanto <dedifardiyanto@yahoo.co.id>
2021-08-08 01:47:04 +0700

    domain

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'cashinout-be.herokuapp.com'),
+    'domain' => env('SESSION_DOMAIN', 'cashinout-be.herokuapp.com/public'),
 
     /*
     |--------------------------------------------------------------------------
